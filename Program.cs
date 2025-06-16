9bec88e9ec8b2cc87d0ddf0fd02de4d2002c1fce
Alex Beletskiy <belletskiy75@mail.ru>
2025-06-16 20:40:46 +0300

    начала программы, вводим с клавиатуры данные

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество строк: ");
+        int n = int.Parse(Console.ReadLine());
+
+        string[] source = new string[n];
+
+        Console.WriteLine("Введите строки:");
+        for (int i = 0; i < n; i++)
+        {
+            source[i] = Console.ReadLine();
+        }
+    }
+}
+
+
+
